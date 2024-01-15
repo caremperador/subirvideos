b2e31e0822fb0a3b184f6789047c8a2736c73690
caremperador <filtrosperu@gmail.com>
2024-01-15 11:49:03 -0500

    Refactor disk space usage display in create.blade.php

--- resources/views/videos/create.blade.php
+++ resources/views/videos/create.blade.php
@@ -70,18 +70,40 @@
     </div>
 
 
- {{-- lista de servidores --}}
- <div class="pt-4">
-    <div class="bg-white p-4 rounded-lg shadow-md">
-        <h2 class="text-xl font-semibold text-gray-800 mb-4">Uso del Espacio en Discos</h2>
-        <ul class="list-disc pl-5 space-y-2">
+    <div class="bg-white shadow overflow-hidden sm:rounded-lg">
+
+        <div class="px-4 py-5 sm:px-6">
+          <h2 class="text-lg leading-6 font-medium text-gray-900">
+            Uso del Espacio en Discos
+          </h2>
+        </div>
+      
+        <div class="border-t border-gray-200">
+          <dl>
+      
             @foreach ($sumasPorDiscoGB as $disco => $tamaño)
-            <li class="text-gray-700">{{ $disco }}: <span class="font-semibold">{{ $tamaño }}</span></li>
-            <li class="text-gray-600">Espacio libre: {{ $espacioLibreDiscoGB[$disco] }}</li>
-        @endforeach
-        </ul>
-    </div>
-</div>
+              <div class="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">
+                  {{ $disco }}
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {{ $tamaño }} GB
+                </dd>
+              </div>
+              <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">
+                  Espacio libre
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {{ $espacioLibreDiscoGB[$disco] }} GB
+                </dd>
+              </div>
+            @endforeach
+      
+          </dl>
+        </div>
+      
+      </div>
     
 
 
