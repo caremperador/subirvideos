8c2710dec7f2428a38534c2fc45957fb7f2890e6
caremperador <filtrosperu@gmail.com>
2024-03-01 22:35:24 -0500

    Agrega configuraciones de volúmenes dinámicos en filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,5 +1,6 @@
 <?php
-// Número total de volúmenes esperados
+
+/* // Número total de volúmenes esperados
 $totalVolumes = 50; // Ajusta este número según la cantidad de volúmenes que planeas tener
 
 // Genera las configuraciones de los discos de volúmenes dinámicamente
@@ -18,7 +19,37 @@ for ($i = 1; $i <= $totalVolumes; $i++) {
         ];
     }
 }
+ */
 
+//archivo filesystems.php
+// Define los volúmenes dinámicamente aquí. Podrías obtener estos desde una variable de entorno o configuración.
+$volumes = [
+    'volume-ams3-01' => '/mnt/volume_ams3_01',
+    'volume-ams3-02' => '/mnt/volume_ams3_02',
+    'volume-ams3-03' => '/mnt/volume_ams3_03',
+    'volume-ams3-04' => '/mnt/volume_ams3_04',
+    'volume-ams3-05' => '/mnt/volume_ams3_05',
+    'volume-ams3-06' => '/mnt/volume_ams3_06',
+    'volume-ams3-07' => '/mnt/volume_ams3_07',
+    'volume-ams3-08' => '/mnt/volume_ams3_08',
+    'volume-ams3-09' => '/mnt/volume_ams3_09',
+    'volume-ams3-10' => '/mnt/volume_ams3_10',
+];
+
+// Filtra y construye dinámicamente los discos de volúmenes solo si existen.
+$volumeDisksConfig = [];
+foreach ($volumes as $name => $path) {
+    if (file_exists($path)) {
+        $volumeDisksConfig[$name] = [
+            'driver' => 'local',
+            'root' => $path,
+            // Aquí ajustas la URL para que apunte al directorio correcto, si es necesario
+            'url' => env('APP_URL') . "/$name", // Asegúrate de que esta ruta sea accesible públicamente si es necesario
+            'visibility' => 'public',
+            'throw' => false,
+        ];
+    }
+}
 
 return [
 
